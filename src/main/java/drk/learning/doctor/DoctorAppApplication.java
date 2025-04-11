14e716095488c02315c51c647e778e99f47064b7
sharifabee <sharifabee.alur@gmail.com>
2025-04-11 16:17:13 +0530

    postman

--- src/main/java/drk/learning/doctor/DoctorAppApplication.java
+++ src/main/java/drk/learning/doctor/DoctorAppApplication.java
@@ -3,6 +3,23 @@ package drk.learning.doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+/*
+ * 
+ * {
+  "did":778899,
+  "name":"Sarojini",
+  "degree":"MD",
+  "fee":1500,
+  "hospital":"KIMS",
+  "timing":"9 A.M - 4 P.M"
+}
+
+
+URL::::http://localhost:8080/drk/hospital/doc/add
+method::::POST
+ */
 @SpringBootApplication
 public class DoctorAppApplication {
 
